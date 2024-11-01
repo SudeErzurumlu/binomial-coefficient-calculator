fe8cbe32057e0b2fce7f8b511761e50813b7e522
Sude <151455137+SudeErzurumlu@users.noreply.github.com>
2024-11-01 23:45:10 +0300

    Create example_usage.m

---
+++ MATLAB/example_usage.m
@@ -0,0 +1,12 @@
+% example_usage.m
+% Example usage of the binomial_coefficient function
+
+% Parameters for binomial coefficient
+n = 5; % Total number of items
+k = 2; % Number of items to choose
+
+% Calculate the binomial coefficient
+result = binomial_coefficient(n, k);
+
+% Display the result
+fprintf('Binomial coefficient C(%d, %d) is: %d\n', n, k, result);
