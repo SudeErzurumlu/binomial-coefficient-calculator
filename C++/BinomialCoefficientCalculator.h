c757a8582272804a95e7bb588454204058c0dca6
Sude <151455137+SudeErzurumlu@users.noreply.github.com>
2024-11-01 23:42:55 +0300

    Create BinomialCoefficientCalculator.h

---
+++ C++/BinomialCoefficientCalculator.h
@@ -0,0 +1,12 @@
+// BinomialCoefficientCalculator.h
+// Header file for the Binomial Coefficient Calculator using Pascal's Triangle
+
+#ifndef BINOMIALCOEFFICIENTCALCULATOR_H
+#define BINOMIALCOEFFICIENTCALCULATOR_H
+
+class BinomialCoefficientCalculator {
+public:
+    int calculate(int n, int k);
+};
+
+#endif // BINOMIALCOEFFICIENTCALCULATOR_H
