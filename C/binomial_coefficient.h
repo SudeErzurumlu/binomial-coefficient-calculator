61a0be62ea8b0ffe94b5ce24945adb039f934eae
Sude <151455137+SudeErzurumlu@users.noreply.github.com>
2024-11-01 23:47:00 +0300

    Create binomial_coefficient.h

---
+++ C/binomial_coefficient.h
@@ -0,0 +1,9 @@
+// binomial_coefficient.h
+// Header file for calculating binomial coefficient using Pascal's Triangle
+
+#ifndef BINOMIAL_COEFFICIENT_H
+#define BINOMIAL_COEFFICIENT_H
+
+int binomial_coefficient(int n, int k);
+
+#endif // BINOMIAL_COEFFICIENT_H
